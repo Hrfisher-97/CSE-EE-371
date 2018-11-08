75070701ef07c52b187aef8c0754871ba41be3ed
NickNagy <33533928+NickNagy@users.noreply.github.com>
2018-11-08 10:51:51 -0800

    Update binarySearch.sv

--- binarySearch.sv
+++ binarySearch.sv
@@ -1,33 +1,47 @@
 `timescale 1ps/1ps
-module binarySearch (L, found, done, index, clk, reset);
-	input logic clk, reset;
-	input logic [7:0] L;
-	output logic [5:0] index;
-	output logic found;
+module DE1_SoC (CLOCK_50, SW, KEY, LEDR, HEX0, HEX1);
+	input logic CLOCK_50;
+	input logic [9:0] SW;
+	input logic [3:0] KEY;
 	
-	output logic done;
+	output logic [9:0] LEDR;
+	output logic [6:0] HEX0, HEX1;
 	
-	logic [7:0] tempArrData, arrData;
-	logic [5:0] tempIndex;
-	logic [4:0] high, low, counter;
-	logic lessThan, greaterThan, memReady, update;
+	logic [4:0] L;
+	logic done; // not used by this module
 	
-	assign length = high - low + 1'b1;
+	binarySearch(.A(SW[7:0]), .found(LEDR[9]), .done, .L, .clk(CLOCK_50), .start(SW[9]), .reset(~KEY[0]));
 	
-	ram32x8 A (.address(index[4:0]), .clock(clk), .data(L), .wren(1'b0), .q(arrData));
+	// add the hex display thingies
+endmodule
+
+// add the start logic
+module binarySearch (A, found, done, L, clk, start, reset);
+	input logic clk, start, reset;
+	input logic [7:0] A;
+	output logic [4:0] L;
+	output logic found, done;
+	
+	logic [7:0] arrData;
+	logic [5:0] index;
+	logic [4:0] tempL, high, low;
+	logic update;
+	
+	ram32x8 A (.address(L[4:0]), .clock(clk), .data(A), .wren(1'b0), .q(arrData));
 
 	assign found = (L == arrData);
 	assign index = (low + high)>>1;
+	assign L = index[4:0];
 	
 	always_ff @(posedge clk) begin
-		tempIndex <= index;
+		tempL <= L;
 		if (reset) begin
 			high <= 5'b11111;
 			low <= 0;
 			done <= 0;
 			update <= 1;
-		end else begin
-			if (tempIndex == index)
+		end else if(start) begin
+			if (tempL == L)
 				update <= 0;
 			else
 				update <= 1;
@@ -36,9 +50,9 @@ module binarySearch (L, found, done, index, clk, reset);
 					done <= 1;
 				else if (update) begin
 					if (L < arrData) begin
-						high <= index - 1'b1;
+						high <= L - 1'b1;
 					end else if (L > arrData) begin
-						low <= index + 1'b1;
+						low <= L + 1'b1;
 					end
 				end
 			end
@@ -49,12 +63,12 @@ endmodule
 
 module binarySearch_testbench();
 	logic clk, reset, found, done;
-	logic [5:0] index;
+	logic [5:0] L;
 	logic [7:0] L;
 	
 	parameter CLOCK_PERIOD = 100;
 	
-	binarySearch dut (.L, .found, .done, .index, .clk, .reset);
+	binarySearch dut (.L, .found, .done, .L, .clk, .reset);
 	
 	initial begin
 		clk <= 0;
@@ -79,7 +93,7 @@ module binarySearch_testbench();
 		while (~done) begin
 			@(posedge clk);
 		end
-		reset <= 1; L <= 120; @(posedge clk); // exists in a nontrivial index of array
+		reset <= 1; L <= 120; @(posedge clk); // exists in a nontrivial L of array
 		@(posedge clk);
 		reset <= 0; @(posedge clk);
 		while (~done) begin
@@ -88,4 +102,4 @@ module binarySearch_testbench();
 		$stop;
 	end
 	
-endmodule  
+endmodule 
